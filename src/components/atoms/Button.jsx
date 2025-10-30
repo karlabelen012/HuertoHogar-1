1042400526727eb711e76c2607bab0f4f255f5c6
karlabelen012 <ka.herrerah@duocuc.cl>
2025-10-30 11:26:27 -0300

    Se actulizara ract Input y Buton
    Se actualizará react Input y Button

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,8 @@
+const Button = ({children, ...props}) => (
+    <button {...props} className="btn btn-primary">
+        {children}
+    </button>
+);
+
+export default Button;
+
---
+++ src/components/atoms/Input.jsx
@@ -0,0 +1,8 @@
+const Input = ({ label, ...props })=>(
+    <div>
+        <label>{label}</label>
+        <input {...props} className="form-control" />
+    </div>
+);
+
+export default Input;
